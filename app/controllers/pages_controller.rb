9876048032ad844deeb5ca91d9d9724fd5270c61
Lydia Soula soulalydia1@gmail.com <soulalydia1@gmail.com>
2022-09-07 16:14:54 +0200

    resolved conflicts with unrelated histories

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,14 +1,4 @@
 class PagesController < ApplicationController
   def home
   end
-
-  def apropos
-  end
-
-  def contacts
-  end
-
-  def portfolio
-  end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
   root to: "pages#home"
-  get "apropos", to: "pages#apropos"
-  get "contacts", to: "pages#contacts"
-  get "portfolio", to: "pages#portfolio"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
